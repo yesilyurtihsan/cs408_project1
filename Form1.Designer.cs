4a16f8e0f6e1d2d55671d808a2ed850ff5241d1f
Tural Novruzov <tural.nt.1@gmail.com>
2022-12-01 15:15:26 +0300

    Commit so far

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.logs_richTextBox = new System.Windows.Forms.RichTextBox();
+            this.port_textBox = new System.Windows.Forms.TextBox();
+            this.port_label = new System.Windows.Forms.Label();
+            this.listen_button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // logs_richTextBox
@@ -40,21 +43,54 @@
             this.logs_richTextBox.TabIndex = 0;
             this.logs_richTextBox.Text = "";
             // 
+            // port_textBox
+            // 
+            this.port_textBox.Location = new System.Drawing.Point(115, 48);
+            this.port_textBox.Name = "port_textBox";
+            this.port_textBox.Size = new System.Drawing.Size(134, 27);
+            this.port_textBox.TabIndex = 1;
+            // 
+            // port_label
+            // 
+            this.port_label.AutoSize = true;
+            this.port_label.Location = new System.Drawing.Point(63, 51);
+            this.port_label.Name = "port_label";
+            this.port_label.Size = new System.Drawing.Size(35, 20);
+            this.port_label.TabIndex = 2;
+            this.port_label.Text = "Port";
+            // 
+            // listen_button
+            // 
+            this.listen_button.Location = new System.Drawing.Point(269, 47);
+            this.listen_button.Name = "listen_button";
+            this.listen_button.Size = new System.Drawing.Size(94, 29);
+            this.listen_button.TabIndex = 3;
+            this.listen_button.Text = "Listen";
+            this.listen_button.UseVisualStyleBackColor = true;
+            this.listen_button.Click += new System.EventHandler(this.listen_button_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(897, 758);
+            this.Controls.Add(this.listen_button);
+            this.Controls.Add(this.port_label);
+            this.Controls.Add(this.port_textBox);
             this.Controls.Add(this.logs_richTextBox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private RichTextBox logs_richTextBox;
+        private TextBox port_textBox;
+        private Label port_label;
+        private Button listen_button;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,8 +1,25 @@
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
 namespace project_server
 {
+    public class Player
+    {
+        public string Username { get; set; }
+    }
+
     public partial class Form1 : Form
     {
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        List<Socket> client_sockets = new List<Socket>();
+        Hashtable socket_player = new Hashtable();
         List<KeyValuePair<string, int>> question_answers = new List<KeyValuePair<string, int>>();
+
+        bool listening = false;
+        bool terminating = false;
+
         public Form1()
         {
             InitializeComponent();
@@ -24,6 +41,87 @@ namespace project_server
                     new KeyValuePair<string, int>(lines[i], Int32.Parse(lines[i+1]));
 
                 this.question_answers.Add(question_answer_pair);
+            }
+        }
+
+        private void listen_button_Click(object sender, EventArgs e)
+        {
+            int serverPort;
+
+            if (Int32.TryParse(port_textBox.Text, out serverPort))
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, serverPort);
+                serverSocket.Bind(endPoint);
+                serverSocket.Listen(2);
+
+                listening = true;
+
+                Thread acceptThread = new Thread(accept_connections);
+                acceptThread.Start();
+
+                this.logs_richTextBox.AppendText("Started listening on port: " + serverPort + "\n");
+
+            }
+            else
+            {
+                this.logs_richTextBox.AppendText("Please check port number \n");
+            }
+        }
+
+        private void accept_connections()
+        {
+            while (listening)
+            {
+                try
+                {
+                    Socket newClient = serverSocket.Accept();
+                    client_sockets.Add(newClient);
+
+                    this.logs_richTextBox.AppendText("A client is connected.\n");
+
+                    Thread receiveThread = new Thread(() => this.receive(newClient)); // updated
+                    receiveThread.Start();
+                }
+                catch
+                {
+                    if (terminating)
+                    {
+                        listening = false;
+                    }
+                    else
+                    {
+                        this.logs_richTextBox.AppendText("The socket stopped working.\n");
+                    }
+
+                }
+            }
+        }
+
+        private void receive(Socket thisClient) // updated
+        {
+            bool connected = true;
+
+            while (connected && !terminating)
+            {
+                try
+                {
+                    Byte[] buffer = new Byte[64];
+                    thisClient.Receive(buffer);
+
+                    string incomingMessage = Encoding.Default.GetString(buffer);
+                    incomingMessage = incomingMessage.Substring(0, incomingMessage.IndexOf("\0"));
+                    this.logs_richTextBox.AppendText("Client: " + incomingMessage + "\n");
+                }
+                catch
+                {
+                    if (!terminating)
+                    {
+                        this.logs_richTextBox.AppendText("A client has disconnected\n");
+                    }
+                    thisClient.Close();
+                    client_sockets.Remove(thisClient);
+                    connected = false;
+                }
             }
         }
     }
